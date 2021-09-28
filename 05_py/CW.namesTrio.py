34a78e71cb6fd42e1c7fd71b1d6ac53fc5391142
Angela Zhang <90888337+apzhang7@users.noreply.github.com>
2021-09-27 22:19:52 -0400

    Update CW.namesTrio.py

--- 05_py/CW.namesTrio.py
+++ 05_py/CW.namesTrio.py
@@ -1,36 +1,56 @@
 #Shriya Anand and Angela Zhang
 # SoftDev
 # Classwork- Class Names
-# POW WOW SUMMARY: When you call the printName() function, it asks you which period you want the student from. Originally, 
-# the code would then provide a random student from the list; after talking to my partner, however, we have changed it so that 
-# the user can input which number student they want from the list.
-
-import random
-#pd1 = ['emma', 'shriya', 'william']
-#pd2 = ['a', 'b', 'c']
-def printName():
-    try:
-        file = open("pd1.txt", "r")
-        contents1 = (file.read())
-       # print (contents1)
-        pd1 = (contents1.split(", "))
-
-        file = open("pd2.txt", "r")
-        contents2 = (file.read())
-       # print (contents2)
-        pd2 = (contents2.split(", "))
-    except: print ("One or more files are not valid")
+# POW WOW SUMMARY : When you call the printName() function, it asks you which period you want the student from.
+# SUMMARY OF TRIO DISCUSSION : Originally, the code would provide a random student from the list or ask you to input a number;
+# after talking to my partner, however, we have changed it so that it reads a file for both first period and second period, makes 
+# the names into a list each, and then spits out a random name from those periods.
+# QUESTIONS
+# Is there a way this can be done with last names as well as first names like "Name, Test"? Since commas wouldn't work 
+# correctly anymore as a splitting device? -- Angela
+# COMMENTS
+# Python is not that much different from java, but it certainly feels like a different language -- Angela
         
-    try:  
+    try:
+        
         period = int(input("Period 1 or 2? (only type number)"))
-        # name_index = int (input("Choose a number from 0 to " ))
-        if period == 1:
-            # print (pd1[name_index])
-             print(pd1[random.randint(0,len(pd1)-1)])
+        
+        if period == 1:           
+            print (dictionary["one"][random.randint(0,len(pd2)-1)])
         elif period == 2:
-            # print (pd2[name_index])
-            print (pd2[random.randint(0,len(pd2)-1)])
+            print (dictionary["two"][random.randint(0,len(pd2)-1)])
         else:
             print ("This input is not valid")
        
     except: print ("This input is not valid")
+
+
+import random # allows you to use random
+
+def printName(): # function 
+    try: # attempts and if it doesn't work it sends an error and doesn't break things
+        file = open("pd1.txt", "r") # just reads "r" the txt 
+        contents1 = (file.read())
+        pd1 = (contents1.split(", ")) # splits it, make it into list
+
+        file = open("pd2.txt", "r") # does same here as ^^^
+        contents2 = (file.read())
+        pd2 = (contents2.split(", "))
+        
+        dictionary = {} # creates a dictionary instance
+        dictionary["one"] = pd1 # puts it into a dictionary under "one" and defines it
+        dictionary["two"] = pd2
+        
+    except: print ("One or more files are not valid") # if there is only one file, or none at all, this message is printed
+        
+    try:  
+        period = int(input("Period 1 or 2? (only type number)")) # asks for number, and turns it from string input to int in one line
+        
+        if period == 1:
+            print (dictionary["one"][random.randint(0,len(pd2)-1)])
+        elif period == 2:  # this is an else if, written as elif
+            print (dictionary["two"][random.randint(0,len(pd2)-1)])
+        else:
+            print ("This input is not valid")
+       
+    except: print ("This input is not valid") # for errors like numbers that aren't 1 or 2
