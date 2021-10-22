78e33c291fa85b6bb569621a2c822caffb5c7462
Angela Zhang <apzhang7@gmail.com>
2021-10-22 08:30:24 -0400

    added table

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -1,7 +1,7 @@
-#Clyde "Thluffy" Sinclair
-#SoftDev  
-#skeleton/stub :: SQLITE3 BASICS
-#Dec 2020 -- The Time of the Rona
+# Team Snaps (Emma Buller, Theo Fahey, Angela Zhang)
+# SoftDev
+# K16 -- All About Database
+# 2021-10-22
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
@@ -13,8 +13,28 @@ db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
 
 #==========================================================
+c.execute("CREATE TABLE roster (name TEXT, age INTEGER, id INTEGER)")
 
+with open('students.csv') as csvfile:
+    reader = csv.reader(csvfile, delimiter= ",")
+    for row in reader:
+        name = row[0]
+        age = row[1]
+        id = row[2]
+        if(age != "age"):
+            command = "INSERT INTO roster VALUES ('" + name + "'," + age + "," + id + ")"
+            c.execute(command);
 
+c.execute("CREATE TABLE classes (code TEXT, mark INTEGER, id INTEGER)")
+with open('courses.csv') as file:
+    r = csv.reader(file, delimiter= ",")
+    for row in r:
+        code = row[0]
+        mark = row[1]
+        id = row[2]
+        if(mark != "mark"):
+            command = "INSERT INTO classes VALUES ('" + code + "'," + mark + "," + id + ")"
+            c.execute(command);
 # < < < INSERT YOUR TEAM'S POPULATE-THE-DB CODE HERE > > >
 
 
@@ -25,5 +45,3 @@ c.execute(command)    # run SQL statement
 
 db.commit() #save changes
 db.close()  #close database
-
-
